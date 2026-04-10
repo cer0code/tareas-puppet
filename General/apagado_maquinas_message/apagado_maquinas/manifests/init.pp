97c864ee0e84c94727ac7ffe43709b9f0cc98d14
Cer0Code <cerocode.eu@gmail.com>
2026-04-10 09:46:08 +0200

    Add files via upload

---
+++ General/apagado_maquinas_message/apagado_maquinas/manifests/init.pp
@@ -0,0 +1,40 @@
+class apagado_maquinas {
+
+# Fichero para que todos los usuarios (locales o de LDAP/NFS) autoricen automáticamente al usuario root a mostrar ventanas de Zenity en su sesión de XFCE
+  file {"/etc/X11/Xsession.d/99allow-root-zenity" :
+           owner => root , group => root , mode => '644' ,
+           source => "puppet:///modules/apagado_maquinas/99allow-root-zenity"
+  }
+
+#Apagado de máquinas por la mediodía.
+        cron { apagado-mediodia:
+#        command => "/sbin/shutdown -h now",
+        command => "export XAUTHORITY=$(find /var/run/lightdm/root/ -type f -name ':0' 2>/dev/null | head -n 1) && export DISPLAY=:0 && /usr/bin/zenity --info --text='El sistema se apagará en 60 segundos. Guarda tu trabajo ahora si no quieres perdelo.' --title='Aviso de Apagado' && /sbin/shutdown -h +1",
+        user => root,
+        hour => 14,
+        minute => 35,
+	ensure => present,
+	}
+
+#Apagado de máquinas por la tarde
+        cron {  apagado-tarde:
+#        command => "/sbin/shutdown -h now",
+        command => "export XAUTHORITY=$(find /var/run/lightdm/root/ -type f -name ':0' 2>/dev/null | head -n 1) && export DISPLAY=:0 && /usr/bin/zenity --info --text='El sistema se apagará en 60 segundos. Guarda tu trabajo ahora si no quieres perdelo.' --title='Aviso de Apagado' && /sbin/shutdown -h +1",
+        user => root,
+        hour => 16,
+        minute => 30,
+        ensure => present,
+        }
+
+#Apagado de máquinas por la noche
+        cron {  apagado-noche:
+#        command => "/sbin/shutdown -h now",
+        command => "export XAUTHORITY=$(find /var/run/lightdm/root/ -type f -name ':0' 2>/dev/null | head -n 1) && export DISPLAY=:0 && /usr/bin/zenity --info --text='El sistema se apagará en 60 segundos. Guarda tu trabajo ahora si no quieres perdelo.' --title='Aviso de Apagado' && /sbin/shutdown -h +1",
+        user => root,
+        hour => 23,
+        minute => 50,
+	ensure => present,
+	}
+
+}
+
